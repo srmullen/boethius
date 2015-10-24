4e73f6056475528ab5973f7640be8a45abdf2d43
Sean Mullen <srmullen13@gmail.com>
2015-10-24 16:41:36 -0600

    parnassus cleanup

--- packages/boethius/examples/parnassus.js
+++ packages/boethius/examples/parnassus.js
@@ -1,40 +1,49 @@
 function run () {
-	var measureLength = 75,
-		measures = 11,
-		lineLength = measureLength * measures;
-
 	var noteFormat = function (n) {
-		var val = n.duration; // replace duration with value
-		n.value = val;
-		delete n.duration;
-		return ["note", n];
-	},
+			var val = n.duration; // replace duration with value
+			n.value = val;
+			delete n.duration;
+			return ["note", n];
+		},
 		createVoice = function (notes) {
 			return palestrina.melody.phrase(
 				_.fill(new Array(notes.length), 1),
-				dorianCpt
+				notes
 			).map(noteFormat);
+		},
+		renderPhrases = function (topVoice, bottomVoice) {
+			var topPhrase = createVoice(topVoice),
+				bottomPhrase = createVoice(bottomVoice),
+				top = ["voice", {value: 0}, topPhrase],
+				bottom = ["voice", {value: 1}, bottomPhrase];
+
+			var layout = scored.layout(["score", {measures: topVoice.length}, [
+				["staff", {timeSig: "4/4", key: "C", measures: 11, lineLength: 750}],
+				["line", {voices: [0]}, [["clef", {value: "treble"}]]],
+				["line", {voices: [1]}, [["clef", {value: "bass"}]]]
+			]]);
+
+			var events = scored.createEvents([top, bottom]);
+			var score = scored.compose(layout, events);
+
+			return scored.render(score);
 		};
 
-	var dorianCpt = ["a4", "a4", "g4", "a4", "b4", "c5", "c5", "b4", "d5", "c#5", "d5"];
-
+	var dorianCptU = ["a4", "a4", "g4", "a4", "b4", "c5", "c5", "b4", "d5", "c#5", "d5"];
+	var dorianCptL = ["d3", "d3", "a3", "f3", "e3", "d3", "f3", "c4", "d4", "c#4", "d4"];
 	var dorianCantus = ["d4", "f4", "e4", "d4", "g4", "f4", "a4", "g4", "f4", "e4", "d4"];
 
-	var dorianCptPhrase = createVoice(dorianCpt),
-		dorianCantusPhrase = createVoice(dorianCantus),
-		counterpoint = ["voice", {value: 0}, dorianCptPhrase],
-		cantusFirmus = ["voice", {value: 1}, dorianCantusPhrase];
+	renderPhrases(dorianCptU, dorianCantus).translate(50, 50);
+
+	var phrygianCantus = ["e4", "c4", "d4", "c4", "a3", "a4", "g4", "e4", "f4", "e4"];
+
+	// Example with two staves.
+	// var layout = scored.layout(["score", {measures: measures}, [
+	// 	["staff", {timeSig: "4/4", key: "C", measures: 6, lineLength: lineLength}],
+	// 	["staff", {lineLength: lineLength, startMeasure: 6, measures: 5}],
+	// 	["line", {voices: [0]}, [["clef", {value: "treble"}]]],
+	// 	["line", {voices: [1]}, [["clef", {value: "bass"}]]]
+	// ]]);
 
 
-	var layout = scored.layout(["score", {measures: measures}, [
-		["staff", {timeSig: "4/4", key: "C", measures: 6, lineLength: lineLength}],
-		["staff", {lineLength: lineLength, startMeasure: 6, measures: 5}],
-		["line", {voices: [0]}, [["clef", {value: "treble"}]]],
-		["line", {voices: [1]}, [["clef", {value: "bass"}]]]
-	]]);
-
-	var events = scored.createEvents([counterpoint, cantusFirmus]);
-	var score = scored.compose(layout, events);
-
-	scored.render(score).translate(50, 50);
 }
