76950d43ba0021085da7395d83c1fb37f8ecdfc0
Sean Mullen <srmullen13@gmail.com>
2016-05-16 18:38:23 -0600

    test setup

---
+++ packages/boethius-lang/test/lang.js
@@ -0,0 +1,10 @@
+import {expect} from "chai";
+import lang from "../lang";
+
+describe("parser", () => {
+    describe("notes", () => {
+        it("should fail", () => {
+            expect(1).to.equal(2);
+        });
+    });
+});
